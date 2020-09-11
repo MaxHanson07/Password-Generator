c040fc0ab606a760a45b83e9d073ecc8283b60b1
Max Hanson <mthanson2@gmail.com>
2020-09-11 15:58:51 -0700

    Seperated logic into functions and made it so password displays on webpage

--- script.js
+++ script.js
@@ -16,24 +16,15 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-// Create prompt to get password length from user
-var passwordLength = prompt("How many characters would you like your password to be?");
 
-// // Create Confirm to ask user if they want lowercase
-var hasLowercase = confirm("Do you want your password to include lowercase characters?");
 
-// // Create Confirm to ask user if they want uppercase
-var hasUppercase = confirm("Do you want your password to include UPPERCASE characters?");
+// TODO Add validations to make sure user chose at least one type of character
 
-// // Create Confirm to ask user if they want number
-var hasNumber = confirm("Do you want your password to include numbers?");
+// TODO Add validation to the length to make sure it is between 8-128 characters
 
-// // Create Confirm to ask user if they want special char
-var hasSpecial = confirm("Do you want your password to include special characters?");
 
-// TODO Create password with specified criteria
 
-// TODO Create array for each type of character (lowercase, numbers, special, etc.)
+// Create array for each type of character (lowercase, numbers, special, etc.)
 
 // Used to split into array
 var lowercaseString = "abcdefghijklmnopqrstuvwxyz";
@@ -48,56 +39,71 @@ var uppercaseArray = uppercaseString.split("");
 var numberArray = numberString.split("");
 var specialArray = specialString.split("");
 
-console.log(uppercaseArray);
-console.log(lowercaseArray);
-console.log(specialArray);
-console.log(numberArray);
-
 // Create new var that holds all chosen char (full array)
 var chosenChar = [];
 
-// Use "if statements" to save chosen characters to the chosen char array
+function generatePassword() {
+  // Create prompt to get password length from user
+  var passwordLength = prompt("How many characters would you like your password to be?");
 
-if (hasLowercase) {
-  contentsToChosen(lowercaseArray);
+  // // Create Confirm to ask user if they want lowercase
+  var hasLowercase = confirm("Do you want your password to include lowercase characters?");
+
+  // // Create Confirm to ask user if they want uppercase
+  var hasUppercase = confirm("Do you want your password to include UPPERCASE characters?");
+
+  // // Create Confirm to ask user if they want number
+  var hasNumber = confirm("Do you want your password to include numbers?");
+
+  // // Create Confirm to ask user if they want special char
+  var hasSpecial = confirm("Do you want your password to include special characters?");
+
+  // TODO Create password with specified criteria
+
+
+
+  // Use "if statements" to save chosen characters to the chosen char array
+
+  if (hasLowercase) {
+    contentsToNewArray(lowercaseArray, chosenChar);
+  }
+
+  if (hasUppercase) {
+    contentsToNewArray(uppercaseArray, chosenChar);
+  }
+
+  if (hasNumber) {
+    contentsToNewArray(numberArray, chosenChar);
+  }
+
+  if (hasSpecial) {
+    contentsToNewArray(specialArray, chosenChar);
+  }
+
+  // Create password based on user's specified criteria
+
+  // Create array to hold the characters of the user's generated password
+  var finalPasswordArray = [];
+
+  //  Use Math.floor(Math.random()) to get a random number
+  //  Use for loop that iterates the same amount as user's specified password length
+  for (var j = 0; j < passwordLength; j++) {
+    var randomSelection = Math.floor(Math.random() * chosenChar.length)
+    finalPasswordArray.push(chosenChar[randomSelection]);
+  }
+
+  // Converts array of characters into a string to display to user
+  password = finalPasswordArray.join("");
+
+  console.log(password);
+
+  return password;
+
 }
-
-if (hasUppercase) {
-  contentsToChosen(uppercaseArray);
-}
-
-if (hasNumber) {
-  contentsToChosen(numberArray);
-}
-
-if (hasSpecial) {
-  contentsToChosen(specialArray);
-}
-
-
-// TODO Create loop to choose characters that match criteria. Choose as many characters as user's specified password length
-//  Use Math.floor(Math.random()) to get a random number
-//  Use for loop that iterates the same amount as user's specified password length
-
-// Create array to hold the characters of the user's generated password
-var finalPasswordArray = [];
-
-
-for (var j = 0; j < passwordLength; j++) {
-  var randomSelection = Math.floor(Math.random() * chosenChar.length)
-  finalPasswordArray.push(chosenChar[randomSelection]);
-}
-
-console.log(finalPasswordArray);
-
-// TODO Add validations to make sure user chose at least one type of character
-
-// TODO Add validation to the length to make sure it is between 8-128 characters
 
 // Create function that adds selected arrays into array of all chosen characters
-function contentsToChosen(selectedArray) {
+function contentsToNewArray(selectedArray, newArray) {
   for (var i of selectedArray) {
-    chosenChar.push(i);
+    newArray.push(i);
   }
 }
-
