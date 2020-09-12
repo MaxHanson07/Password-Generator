3a87160b14686dae4adbe3c3dfa1cbdeeb7b4cf7
Max Hanson <mthanson2@gmail.com>
2020-09-11 18:36:02 -0700

    Added validation. Broke code up into smaller functions. Added comments.

--- script.js
+++ script.js
@@ -13,17 +13,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-
-
-
-// TODO Add validations to make sure user chose at least one type of character
-
-// TODO Add validation to the length to make sure it is between 8-128 characters
-
-
-
 // Create array for each type of character (lowercase, numbers, special, etc.)
 
 // Used to split into array
@@ -31,7 +20,6 @@ var lowercaseString = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numberString = "0123456789";
 var specialString = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-
 
 // Creates arrays with each character type
 var lowercaseArray = lowercaseString.split("");
@@ -42,68 +30,112 @@ var specialArray = specialString.split("");
 // Create new var that holds all chosen char (full array)
 var chosenChar = [];
 
+// Variables storing values based on user response. Booleans are marked false until user confirms them true
+var passwordLength
+var hasLowercase = false;
+var hasUppercase = false;
+var hasNumber = false;
+var hasSpecial = false;
+
 function generatePassword() {
-  // Create prompt to get password length from user
-  var passwordLength = prompt("How many characters would you like your password to be?");
 
-  // // Create Confirm to ask user if they want lowercase
-  var hasLowercase = confirm("Do you want your password to include lowercase characters?");
-
-  // // Create Confirm to ask user if they want uppercase
-  var hasUppercase = confirm("Do you want your password to include UPPERCASE characters?");
-
-  // // Create Confirm to ask user if they want number
-  var hasNumber = confirm("Do you want your password to include numbers?");
-
-  // // Create Confirm to ask user if they want special char
-  var hasSpecial = confirm("Do you want your password to include special characters?");
-
-  // TODO Create password with specified criteria
-
-
+  // Get desired password details from user
+  passwordLength = getLength();
+  getCriteria();
 
   // Use "if statements" to save chosen characters to the chosen char array
-
   if (hasLowercase) {
     contentsToNewArray(lowercaseArray, chosenChar);
   }
-
   if (hasUppercase) {
     contentsToNewArray(uppercaseArray, chosenChar);
   }
-
   if (hasNumber) {
     contentsToNewArray(numberArray, chosenChar);
   }
-
   if (hasSpecial) {
     contentsToNewArray(specialArray, chosenChar);
   }
 
   // Create password based on user's specified criteria
-
-  // Create array to hold the characters of the user's generated password
-  var finalPasswordArray = [];
-
-  //  Use Math.floor(Math.random()) to get a random number
-  //  Use for loop that iterates the same amount as user's specified password length
-  for (var j = 0; j < passwordLength; j++) {
-    var randomSelection = Math.floor(Math.random() * chosenChar.length)
-    finalPasswordArray.push(chosenChar[randomSelection]);
-  }
-
-  // Converts array of characters into a string to display to user
-  password = finalPasswordArray.join("");
-
-  console.log(password);
+  password = randomizePassword(chosenChar);
 
   return password;
-
 }
 
-// Create function that adds selected arrays into array of all chosen characters
+// Gets desired password length from user
+function getLength() {
+  // Create prompt to ask user how long they want password
+  passwordLength = prompt("How many characters would you like your password to be?");
+
+  // Validates that password is adequate length
+  if ((passwordLength < 8) || (passwordLength > 128)) {
+    var longEnough = false;
+  }
+
+  // Requires user to choose new length until meeting requirements
+  while (longEnough === false) {
+    passwordLength = prompt("Password must be no less than 8 characters and no more than 128 characters");
+
+    if ((passwordLength >= 8) && (passwordLength <= 128)) {
+      longEnough = true;
+    }
+  }
+
+  return passwordLength;
+}
+
+// Gets desired character type criteria from user
+function getCriteria() {
+
+  // Local variable holds boolean indicating whether user has provided adequate password criteria
+  var enoughInfo = false;
+
+  // While loop validates user's answers. Password must include at least 1 type of characters
+  while (enoughInfo === false) {
+
+    // Create Confirm to ask user if they want lowercase
+    hasLowercase = confirm("Do you want your password to include lowercase characters?");
+
+    // Create Confirm to ask user if they want uppercase
+    hasUppercase = confirm("Do you want your password to include UPPERCASE characters?");
+
+    // Create Confirm to ask user if they want numbers
+    hasNumber = confirm("Do you want your password to include numbers?");
+
+    // Create Confirm to ask user if they want special characters
+    hasSpecial = confirm("Do you want your password to include special characters?");
+
+    // Stops while loop once password length is adequate and at least one character type is selected
+    if (hasLowercase || hasUppercase || hasNumber || hasSpecial) {
+      enoughInfo = true;
+    }
+    else {
+      // Tell user they need to choose at least 1 type of characters
+      alert("Password must include at least 1 type of characters");
+    }
+  }
+}
+
+// Create function that adds selected arrays into array of all chosen characters instead of an array of arrays
 function contentsToNewArray(selectedArray, newArray) {
   for (var i of selectedArray) {
     newArray.push(i);
   }
+}
+
+// Randomly orders characters based on the specified parameters provided by user
+function randomizePassword(criteria) {
+  // Create array to hold the characters of the user's generated password
+  var finalPasswordArray = [];
+
+  //  Use Math.floor(Math.random()) to get a random number to choose a random character from chosenChar array
+  //  Use for loop that iterates the same amount as user's specified password length
+  for (var j = 0; j < passwordLength; j++) {
+    var randomSelection = Math.floor(Math.random() * criteria.length)
+    finalPasswordArray.push(criteria[randomSelection]);
+  }
+
+  // Converts array of characters into a string to display to user
+  return finalPasswordArray.join("");
 }
